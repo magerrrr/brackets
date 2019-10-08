68b7fbb1cbb2d8678b94df68aac2dc2b7c1d0704
Kirill Mager <kiruha24@mail.ru>
2019-10-08 23:07:31 +0300

    feat: add new solution (passed 20 tests from 20)

--- src/index.js
+++ src/index.js
@@ -1,78 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-    function isOpen(symbol, config){ // return state open/closed
-        if (symbol === config[0]) {
-            return true;
-        } 
-        return false;
-    }
-
-    function isGlobalOpen(symbol, configs) {
-        for (let i = 0; i < configs.length ; i++) {
-            if (isOpen(symbol, configs[i])) {
-                return true;
-            }
+module.exports = function check(str, configs) {
+    let expression = new String(str);
+    let isChanged = true;
+    while(isChanged) {
+      isChanged = false;
+      for (let i = 0; i < expression.length-1; i++) {
+        let first = expression[i];
+        let second = expression[i+1];
+  
+        let currentPair = first.concat(second);
+  
+        for (let j = 0; j < configs.length; j++) {
+          const config = configs[j];
+          const configPair = config[0] + config[1];
+          if(currentPair === configPair) {
+            isChanged = true;
+            expression = expression.replace(configPair,'');
+          }
         }
-        return false;
+      }
     }
-    function isValid(opened, closed, configs) {
-        let i = getConfig(opened, configs);
-
-
-        if(i===-1) {
-            return false;
-        }
-
-        if (closed === configs[i][1]) {
-            return true;
-        }
-        return false;
-    }
-
-    function getConfig(opened, configs) {
-        for (let i = 0; i < configs.length; i++) {
-            if (opened === configs[i][0]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    function sameElements(stack) {
-        if(stack.length>=2) {
-            if(stack[stack.length-1] == stack[stack.length -2]) {
-                stack.pop();
-                return true;
-            }
-        }
-        return false;
-        
-    }
-    let stack = [];
-    for (let i = 0; i < str.length; i++) {
-        if (isGlobalOpen(str[i], bracketsConfig)){
-            stack.push(str[i]);
-
-            if(sameElements(stack)) {
-                
-            }
-        } else {
-            let lastStackElement = stack.pop();
-
-            if (typeof lastStackElement === 'undefined') {
-                return false;
-            }
-
-            if (!isValid(lastStackElement, str[i], bracketsConfig) ) {
-                return false;
-            }
-        }
-    }
-
-
-    if (stack.length === 0) {
-        return true;
-    }
+  
+    if (expression.length==0){
+      return true;
+    } 
     return false;
-
-}
-    
+  }
